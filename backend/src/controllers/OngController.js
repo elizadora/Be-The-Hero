f5e729f71a6b74c8cca534b929e1d4598a7e5f0e
elizadora <elizadoradasilva2003@gmail.com>
2022-10-03 15:46:44 -0300

    Deletar ONG

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -25,5 +25,13 @@ module.exports = {
         });
 
         return response.json({id});
+    },
+
+    async delete(request, response){
+        const { id } = request.params;
+
+        await connection('ongs').where('id',id).delete();
+
+        return response.status(204).send();
     }
 }
